987a654c754f174fe631edb446586eadce98751a
akshaynew2011 <akshaynew2011@hotmail.com>
2018-06-09 18:14:03 +0530

    initial commit

---
+++ Derby_Counter/app/src/main/java/com/example/aksha/derby_counter/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.aksha.derby_counter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
